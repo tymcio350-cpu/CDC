e27d33c1350db1841eff9962d677cc83b47162ad
xZurek <tymcio350@gmail.com>
2025-09-24 22:15:40 +0200

    vite.config.mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,19 +1,11 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Ustaw base na nazwę repo w GitHub Pages: "/REPO_NAME/"
-// W Twoim przypadku: "/CDC/"
+
 export default defineConfig({
-  base: '/CDC/',
-  plugins: [react()],
-  server: {
-    port: 5173,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: false,
-    // opcjonalnie: możesz ustawić terserOptions / rollupOptions tu, jeśli potrzebujesz
-  }
+plugins: [react()],
+server: {
+port: 5173,
+open: true
+}
 })
